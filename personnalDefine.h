abe15bd51b203df0732ebfe4980824851946ecbd
yuc <yuc55577@gmail.com>
2023-12-14 11:09:28 +0800

    [新增]轮子状态码

---
+++ personnalDefine.h
@@ -0,0 +1,17 @@
+#ifndef __PERSONNALDEFINE_H_
+#define __PERSONNALDEFINE_H_
+
+
+
+/* 8XXX 函数中判断出错的返回值 */
+#define NULL_POINT          8000        //空指针
+#define MALLOC_ERROR        8001        //堆空间申请失败
+#define INSERT_INDEX_VALID  8002        //插入的位置非法
+#define DELETE_INDEX_VALID  8003        //删除的位置非法
+
+#define DLLIST_EMPTY        8004        //双链表中没有数据
+
+
+
+
+#endif
