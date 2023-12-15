9013bee7c465cff86f51c0af9ec2b37f122912a2
gentelyu <152053215+gentelyu@users.noreply.github.com>
2023-12-15 11:48:19 +0800

    Delete 封库的安全用例 directory

--- 封库的安全用例/chen.h
+++
@@ -1,9 +0,0 @@
-#ifndef __CHEN_H_
-#define __CHEN_H_
-
-
-typedef struct CHEN chen;
-
-int initchen(chen * db);
-
-#endif
--- 封库的安全用例/yu.h
+++
@@ -1,9 +0,0 @@
-#ifndef __YU_H_
-#define __YU_H_
-
-typedef struct YU yu;
-
-int initYU(yu *db);
-
-
-#endif
